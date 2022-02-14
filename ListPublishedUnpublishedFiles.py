3903991aa4000e0fb38db6b8a229a168ead52d79
Weber <Weber.Marc@epa.gov>
2022-02-14 08:35:51 -0800

    list published unpublished for LakeCat

--- ListPublishedUnpublishedFiles.py
+++ ListPublishedUnpublishedFiles.py
@@ -5,21 +5,23 @@ from ftplib import FTP
 
 ftp = FTP('newftp.epa.gov')
 ftp.login()
-ftp.cwd("/EPADataCommons/ORD/NHDPlusLandscapeAttributes/StreamCat/HydroRegions/")
+ftp.cwd("/EPADataCommons/ORD/NHDPlusLandscapeAttributes/LakeCat/FinalTables")
 
 
 published = ftp.nlst()
 
-local_list = os.listdir(r'O:\PRIV\CPHEA\PESD\COR\CORFiles\Geospatial_Library_Projects\StreamCat\FTP_Staging\HydroRegions\zips')
+local_list = os.listdir(r'O:\PRIV\CPHEA\PESD\COR\CORFiles\Geospatial_Library_Projects\LakeCat\FTP_Staging\FinalTables\zips')
 
 local_published =  [x for x in local_list if x in published]
 local_unpublished =  [x for x  in local_list if x not in published]
 
-control = pd.read_csv(r'E:\GitProjects\StreamCat\ControlTable_StreamCat.csv')            
+control = pd.read_csv(r'E:\GitProjects\LakeCat\ControlTable_LakeCat.csv')            
 local_published_metrics =  [elem.split('_Region')[0] for elem in local_published]
 local_published_metrics = list(set(local_published_metrics))
+
+local_published_metrics = [os.path.splitext(x)[0] for x in local_published_metrics]
 
 list(control)
 control['Published'] = control['Final_Table_Name'].isin(local_published_metrics)
 control['Published'] = control['Published'].map({True: 'Yes', False: 'No'})
-control.to_csv(r'E:\GitProjects\StreamCat\ControlTable_StreamCat.csv', index=False)
+control.to_csv(r'E:\GitProjects\LakeCat\ControlTable_LakeCat.csv', index=False)
