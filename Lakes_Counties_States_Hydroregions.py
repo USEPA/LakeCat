55a15d862de722ff00ef3816f83eae979f2f36ef
Weber <Weber.Marc@epa.gov>
2024-05-20 12:52:14 -0700

    intersect lakes

--- Lakes_Counties_States_Hydroregions.py
+++ Lakes_Counties_States_Hydroregions.py
@@ -13,7 +13,8 @@ wbdy = wbdy.loc[wbdy["FTYPE"].isin(["LakePond", "Reservoir"])]
 
 cnty = gpd.read_file('O:/PRIV/CPHEA/PESD/COR/CORFILES/Geospatial_Library_Resource/DEMOGRAPHIC/BOUNDARIES/NATIONAL/tl_2018_us_county.shp').to_crs(epsg="5070")
 
-wbdy_cnty = 
+wbdy_cnty = gpd.sjoin(left_df=wbdy[['COMID','GNIS_ID','GNIS_NAME','REACHCODE','geometry']], right_df=cnty[['STATEFP','COUNTYFP','GEOID','geometry']], how="left", predicate="intersects")
+
 
 hydrgn
 
