1e6f56737bb606d95b7ffad2796259e2d4def1f5
Weber <Weber.Marc@epa.gov>
2024-05-20 16:03:23 -0700

    write out lookups to .csv on O drive

--- Lakes_Counties_States_Hydroregions.py
+++ Lakes_Counties_States_Hydroregions.py
@@ -6,6 +6,7 @@ Created on Mon May 20 11:37:20 2024
 """
 
 import geopandas as gpd
+import pandas as pd
 
 wbdy = gpd.read_file("G:/NHDPlusV21/NHDPlusNationalData/NHDPlusV21_National_Seamless_Flattened_Lower48.gdb",
                      layer="NHDWaterbody",driver = 'FileGDB').to_crs(epsg="5070")
@@ -18,5 +19,10 @@ wbdy_cnty = gpd.sjoin(left_df=wbdy[['COMID','GNIS_ID','GNIS_NAME','REACHCODE','g
 hydrgn = gpd.read_file("G:/NHDPlusV21/NHDPlusGlobalData/BoundaryUnit.shp").to_crs(epsg="5070")
 hydrgn = hydrgn.loc[hydrgn["UnitType"]=='VPU']
 
-wbdy_hydrgn = gpd.sjoin(left_df=wbdy[['COMID','GNIS_ID','GNIS_NAME','REACHCODE','geometry']], right_df=hydrgn[['DrainageID','UnitID','UnitName','geometry']], how="left", predicate="intersects")   
-    
+wbdy_hydrgn = gpd.sjoin(left_df=wbdy[['COMID','GNIS_ID','GNIS_NAME','REACHCODE','geometry']], right_df=hydrgn[['DrainageID','UnitID','UnitName','geometry']], how="left", predicate="intersects") 
+
+wbdy_cnty = pd.DataFrame(wbdy_cnty.drop(columns='geometry'))
+wbdy_hydrgn = pd.DataFrame(wbdy_hydrgn.drop(columns='geometry'))  
+
+wbdy_cnty.to_csv('O:/PRIV/CPHEA/PESD/COR/CORFILES/Geospatial_Library_Projects/LakeCat/LakeCat_County_State.csv', index=False)  
+wbdy_hydrgn.to_csv('O:/PRIV/CPHEA/PESD/COR/CORFILES/Geospatial_Library_Projects/LakeCat/LakeCat_HydroRegion.csv', index=False)  
